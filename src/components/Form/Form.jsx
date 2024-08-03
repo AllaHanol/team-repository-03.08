a9839235e4a93245f759dd346e553c3c4bad1f5a
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-08-03 10:47:42 +0200

    task1 all

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,38 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
+import { useState } from 'react';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({ onSubmit }) => {
+ 
+  const [searchQuery, setSearchQuery] = useState('');
+  const handleChange = e => {
+    setSearchQuery(e.target.value);
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!searchQuery.trim()) {
+      return alert('Please enter a search query');
+    }
+
+    onSubmit(searchQuery);
+    setSearchQuery('');
+    
+  }
+
+
+  return <form onSubmit={handleSubmit} className={style.form}>
+  <button className={style.button} type="submit">
+    <FiSearch size="16px" />
+  </button>
+
+  <input value ={searchQuery} onChange={handleChange}
+    className={style.input}
+    placeholder="What do you want to write?"
+    name="search"
+    required
+    autoFocus
+  />
+</form>
 };
+
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,19 @@
 // import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { Form, Text } from 'components';
+import { useState } from 'react';
 
 export const Photos = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const handleSubmit = (value) => { 
+    
+    setQuery(value);
+  
+  };
+  
   return (
     <>
+      <Form onSubmit={handleSubmit}/>
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
